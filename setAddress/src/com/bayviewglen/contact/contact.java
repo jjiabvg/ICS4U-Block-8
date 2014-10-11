b9883bf6dabc0ecac10c756eece9e7d9cc444d61
jjia <jjia@bayviewglen.ca>
2014-10-10 20:49:27 -0400

    ,

--- setAddress/src/com/bayviewglen/contact/contact.java
+++ setAddress/src/com/bayviewglen/contact/contact.java
@@ -3,12 +3,11 @@ package com.bayviewglen.contact;
 public class contact {
        private String lastName;
        private String firstName;
-       private String phoneNumber;
+    
        
               public contact(){
                      lastName = null;
                      firstName = null;
-                     phoneNumber = null;
               }
               
               public contact(String last, String first){
@@ -24,12 +23,10 @@ public class contact {
                      return firstName;
               }
               
-              public String getPhoneNumber(){
-                     return phoneNumber;
-              }
+           
 
 			public void getContact() {
-				System.out.println(phoneNumber + " " + firstName + " " + lastName);
+				System.out.println( firstName + " " + lastName);
 				
 			}
               
